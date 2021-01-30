8e4dc856d11e417ac04bf162fd531da0f82f9821
ilian <ileto02@abv.bg>
2021-01-31 00:06:50 +0200

    post and comment entities
    same as title

---
+++ soon-to-be-named/soon-to-be-named/Models/Entities/CommentEntity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace soon_to_be_named.Models.Entities
+{
+    public class CommentEntity
+    {
+
+        public int ID { get; set; }
+        public DateTime Date { get; set; }
+        public String PosterName { get; set; }
+        public String Description { get; set; }
+        public double Stars { get; set; }
+
+
+    }
+}
