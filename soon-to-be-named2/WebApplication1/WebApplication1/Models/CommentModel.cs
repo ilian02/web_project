32f7a527d36073a7a34375c9145a584280b115f2
ilian <ileto02@abv.bg>
2021-02-06 22:25:49 +0200

    sidebar menu
    sidebar menu

--- soon-to-be-named2/WebApplication1/WebApplication1/Models/CommentModel.cs
+++ soon-to-be-named2/WebApplication1/WebApplication1/Models/CommentModel.cs
@@ -11,7 +11,9 @@ namespace WebSite.Models
         [Key]
         public int ID { get; set; }
         public String PosterName { get; set; }
+        [StringLength(250, ErrorMessage = "Limit yourself to 250 lenght")]
         public String Description { get; set; }
+        [Range(1, 5, ErrorMessage = "Between 1 to 5")]
         public double Stars { get; set; }
         public int PostID { get; set; }
     }
