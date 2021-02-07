02b429ed1f592347ab7da9a184d9a1e76748129b
ilian <ileto02@abv.bg>
2021-02-07 10:50:40 +0200

    minor changes
    minor changes

--- soon-to-be-named2/WebApplication1/WebApplication1/Controllers/HomeController.cs
+++ soon-to-be-named2/WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -23,7 +23,7 @@ namespace WebApplication1.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult Contact()
         {
             return View();
         }
